6f3ef84f13231345f8b5d4e2522174d89d4ec939
fpvlvr <fpv-lover@proton.me>
2025-11-20 02:45:47 +0100

    next fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,9 +3,3 @@ output "connection_uri" {
   description = "Public Postgres connection string"
   sensitive   = true
 }
-
-output "database_host" {
-  value       = neon_project.esclavizador_project.host
-  description = "Database hostname"
-  sensitive   = true
-}
